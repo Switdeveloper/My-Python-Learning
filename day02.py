b491881c46ebb506182c13a6b70bd6bc613c4f98
Switdeveloper <102440846+Switdeveloper@users.noreply.github.com>
2026-09-04 22:03:13 +0100

    new

--- day02.py
+++ day02.py
@@ -1,15 +1,14 @@
+# TODO: Read the user's age from input
+
+
+# TODO: Print: You are <age> years old!
+
 def main():
-    # Ask the user for their age
-    age_input = input("Enter your age: ")
-
-    # Convert the input to an integer
-    age = int(age_input)
-
-    # Create a greeting message using an f-string
-    message = f"You are {age} years old!"
-
-    # Print the message
-    print(message)
+    age = input("Enter your age: ")
+    print(f"You are {age} years old!")
+    
+    # TODO: print the greeting using the age variable
+    pass
 
 if __name__ == "__main__":
     main()
